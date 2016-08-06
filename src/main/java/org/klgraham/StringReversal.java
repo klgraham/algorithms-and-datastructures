1e6302eeabc1119a091868622712fb41f6ba07f8
Kenneth Graham <klgraham@gmail.com>
2016-08-05 21:05:20 -0500

    Added recursive string reversal

---
+++ src/main/java/org/klgraham/StringReversal.java
@@ -0,0 +1,38 @@
+package org.klgraham;
+
+/**
+ * Created by klogram on 8/5/16.
+ */
+public class StringReversal {
+
+    public static String reverseStringIterative(final String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = s.length()-1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
+    public static String reverseStringRecursive(final String s) {
+        int charCount = s.length();
+        return accumulateChars(s, charCount, new StringBuilder());
+    }
+
+    private static String accumulateChars(final String s, int remainingChars, final StringBuilder accumulator) {
+        if (remainingChars == 0) return accumulator.toString();
+
+        accumulator.append(s.charAt(remainingChars - 1));
+        return accumulateChars(s, remainingChars - 1, accumulator);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "This is a test. This is only a test.";
+
+        System.out.println("String: " + s1);
+        System.out.println("Reversed string (iterative): " + reverseStringIterative(s1));
+        System.out.println("Reversed string (recursive): " + reverseStringRecursive(s1));
+
+    }
+}
