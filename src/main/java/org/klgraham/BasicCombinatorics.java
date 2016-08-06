cb24377c578ab1e3b458b180f593f28cc4d9b88e
Kenneth Graham <klgraham@gmail.com>
2016-08-05 21:33:49 -0500

    Renamed class

---
+++ src/main/java/org/klgraham/BasicCombinatorics.java
@@ -0,0 +1,46 @@
+package org.klgraham;
+
+/**
+ * Description of file content.
+ *
+ * @author kgraham
+ *         8/5/16
+ */
+public class BasicCombinatorics
+{
+	public static int factorial(int n)
+	{
+		// Java 8 streams are not fast yet, so simple iteration is the way to go
+		int result = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			result += i;
+		}
+		return result;
+	}
+
+	// Java does not have tail-call optimization, so this will not work for large n
+	public static int recursiveFactorial(int n)
+	{
+		if (n > 0)
+		{
+			return n * recursiveFactorial(n - 1);
+		} else
+		{
+			return 1;
+		}
+	}
+
+	public static double binomial(int n, int m)
+	{
+		return factorial(n) / (factorial(m) * factorial(n-m));
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("5! = " + BasicCombinatorics.recursiveFactorial(5));
+		System.out.println("1000! = " + BasicCombinatorics.factorial(1000));
+		System.out.println("10 choose 3 = " + BasicCombinatorics.binomial(10, 3));
+
+	}
+}
