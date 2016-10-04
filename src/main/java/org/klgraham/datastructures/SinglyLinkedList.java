0a02cc76535736657344058da2b6f5d965a83616
Kenneth Graham <klgraham@gmail.com>
2016-10-04 00:05:56 -0500

    Started implementing a singly linked list

---
+++ src/main/java/org/klgraham/datastructures/SinglyLinkedList.java
@@ -0,0 +1,72 @@
+package org.klgraham.datastructures;
+
+import java.util.Optional;
+
+/**
+ * Created by klogram on 10/3/16.
+ */
+public class SinglyLinkedList<T extends Comparable<T>> {
+    private Node<T> head;
+    private Node<T> current;
+
+    public SinglyLinkedList(T item) {
+        Node<T> node = new Node<T>(item);
+        this.head = node;
+        current = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void append(T item) {
+        Node<T> node = new Node<T>(item);
+        if (this.isEmpty()) {
+            head = node;
+        }
+        else {
+            Node n = head;
+            while (n.next.isPresent()) {
+                n = (Node) n.getNext().get();
+            }
+
+            n.next = Optional.of(node);
+        }
+    }
+
+    public void insert(T item) {
+        Node<T> previous = head;
+        Node<T> node = new Node<T>(item);
+
+        Node<T> n = head;
+        while (n.item.compareTo(item) < 0) {
+            previous = previous.next.get();
+        }
+
+        // previous will now be the node < item
+        Node<T> nextNode = previous.next.get();
+
+        previous.next = Optional.of(node);
+        node.next = Optional.of(nextNode);
+    }
+
+    
+
+    class Node<T> {
+        private T item;
+        private Optional<Node<T>> next;
+
+        public Node(T item) {
+            this.item = item;
+            this.next = null;
+        }
+
+        public T getItem() {
+            return item;
+        }
+
+        public Optional<Node<T>> getNext() {
+            return next;
+        }
+    }
+}
