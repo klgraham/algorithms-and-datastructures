c14986e65208e95165ee2b0c66353b7afde6c19a
Kenneth Graham <klgraham@gmail.com>
2016-11-19 00:43:06 -0600

    Updated test with improved Groovy syntax

--- src/test/java/org/klgraham/puzzles/AppleStocksTest.groovy
+++ src/test/java/org/klgraham/puzzles/AppleStocksTest.groovy
@@ -9,36 +9,36 @@ class AppleStocksTest extends Specification {
 
     def "Can compute correct best profit" () {
         given:
-        def stockPrices1 =  [10, 7, 5, 8, 11, 9]
-        def stockPrices2 = [10, 7, 5, 8, 11, 9, 3, 2, 1]
+        def stockPrices1 =  [10, 7, 5, 8, 11, 9] as int[]
+        def stockPrices2 = [10, 7, 5, 8, 11, 9, 3, 2, 1] as int[]
 
         expect:
-        6 == AppleStocks.getMaxProfit(stockPrices1.toArray(new Integer[0]))
-        6 == AppleStocks.getMaxProfit(stockPrices2.toArray(new Integer[0]))
+        6 == AppleStocks.getMaxProfit(stockPrices1)
+        6 == AppleStocks.getMaxProfit(stockPrices2)
     }
 
     def "If prices always go down, best profit is negative" () {
         given: "decreasing prices"
-        def prices = [34, 30, 25, 23, 20, 17, 15, 12, 8, 6, 4, 2, 1]
+        def prices = [34, 30, 25, 23, 20, 17, 15, 12, 8, 6, 4, 2, 1] as int[]
 
         expect:
-        AppleStocks.getMaxProfit(prices.toArray(new Integer[0])) >= 0
+        AppleStocks.getMaxProfit(prices) >= 0
     }
 
     def "If prices don't change, best profit is 0"(){
         given: "constant prices"
-        def prices = [3,3,3,3,3,3]
+        def prices = [3,3,3,3,3,3] as int[]
 
         expect:
-        AppleStocks.getMaxProfit(prices.toArray(new Integer[0])) >= 0
+        AppleStocks.getMaxProfit(prices) >= 0
     }
 
     def "Need at least two prices"(){
         given: "constant prices"
-        def prices = [3]
+        def prices = [3] as int[]
 
         when:
-        AppleStocks.getMaxProfit(prices.toArray(new Integer[0]))
+        AppleStocks.getMaxProfit(prices)
 
         then:
         thrown(IllegalArgumentException)
